9aeb0e400b417ed90e53fa55eeb35ae46bec2936
Jennifer Hsiao <etismeimet@gmail.com>
2020-02-21 16:12:31 -0800

    working on param optimization code, also created func.py to store relevant hand-made functions
    Former-commit-id: 88c6df8074290d7f514ee1d0b001793d747ffc86

---
+++ notebooks/funcs.py
@@ -0,0 +1,12 @@
+# functions for file 'opt_param'
+
+def unfold(val, min, max):
+    """
+    transform values normalized between 0-1 back to their regular range
+    """
+    unfold_list = []
+    for i in val: 
+        unfold_i = i*(max - min) + min
+        unfold_list.append(unfold_i)
+        
+    return unfold_list
