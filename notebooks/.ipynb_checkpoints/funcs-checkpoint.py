75553189aceecda57fb7d3de2f972da08156431f
Jennifer Hsiao <etismeimet@gmail.com>
2020-03-02 11:30:42 -0800

    reset to previous timepoint and trying again
    Former-commit-id: 7ae821ed5513120273ec1442afa2bbff8c08f02a

--- notebooks/.ipynb_checkpoints/funcs-checkpoint.py
+++ notebooks/.ipynb_checkpoints/funcs-checkpoint.py
@@ -1,5 +1,9 @@
 # functions for file 'opt_param'
 
+def tup_convert(tup):
+    str = ''.join(tup)
+    return str
+
 def unfold(val, min, max):
     """
     transform values normalized between 0-1 back to their regular range
@@ -7,6 +11,6 @@ def unfold(val, min, max):
     unfold_list = []
     for i in val: 
         unfold_i = i*(max - min) + min
-        unfold_list.append(unfold_i)
-        
+        unfold_list.append(unfold_i)        
     return unfold_list
+
