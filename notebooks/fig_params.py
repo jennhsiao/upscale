e917df0cb983eca89175f0eff51c56c6fde990a3
Jennifer Hsiao <etismeimet@gmail.com>
2020-09-22 20:09:21 -0700

    streamlining and modualizing code - moving parts into functions or .py files

---
+++ notebooks/fig_params.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from funcs import fold
+
+# my functions
+from palettable.colorbrewer.sequential import YlGnBu_8
+
+# setting parameter range
+df_params = pd.read_csv('/home/disk/eos8/ach315/upscale/inits/param_100.csv')
+df_params = df_params.drop(['rmax_ltar'], axis=1)
+
+param_range = {'juv_leaves': [15, 25],
+               'stayGreen': [3,8],
+               'rmax_ltir': [0.5, 1.5],
+               'phyllo': [2, 4],
+               'LM_min': [80, 120],
+               'Vcmax': [60, 80],
+               'Vpmax': [60, 120],
+               'g1': [1, 10], 
+               'ref_potential': [-4, -1]}
+
+params = df_params.columns
+df_params_fold = pd.DataFrame(columns=params)
+for i in params: 
+    df_params_fold[i] = fold(df_params[i], param_range[i][0], param_range[i][1])
+df_params_trans = pd.DataFrame(df_params_fold.transpose())
+
+fig, ax = plt.subplots(figsize=(28,5))
+ax = sns.heatmap(df_params_trans, cmap=YlGnBu_8.mpl_colormap)
+
+plt.xticks(fontweight='light', fontsize=12)
+plt.yticks(rotation=0, fontweight='light', fontsize=12)
+
+# adjusting color bar specifics
+cbar = ax.collections[0].colorbar
+cbar.ax.tick_params(labelsize=10)
+#fig.colorbar(ax, cax=cbar)
+
+fig.subplots_adjust(left=0.2, bottom=0.45)
+#plt.savefig('/home/disk/eos8/ach315/upscale/figs/params_100.png', format='png', dpi=800, transparent=True)
--- notebooks/funcs.py
+++ notebooks/funcs.py
@@ -1,34 +1,42 @@
-# functions for file 'opt_param'
+import os
+import pandas as pd 
+
 
 def tup_convert(tup):
     str = ''.join(tup)
     return str
 
-def unfold(val, min, max):
-    """
-    transform values normalized between 0-1 back to their regular range
-    """
-    unfold_list = []
-    for i in val: 
-        unfold_i = i*(max - min) + min
-        unfold_list.append(unfold_i)        
-    return unfold_list
 
 def fold(val, min, max):
     """
     transform values normalized between 0-1 back to their regular range
     """
+    
     fold_list = []
     for i in val: 
         fold_i = (i-min)/(max - min)
         fold_list.append(fold_i)        
     return fold_list
 
+
+def unfold(val, min, max):
+    """
+    transform values normalized between 0-1 back to their regular range
+    """
+    
+    unfold_list = []
+    for i in val: 
+        unfold_i = i*(max - min) + min
+        unfold_list.append(unfold_i)        
+    return unfold_list
+
+
 def get_filelist(path):
     """
-    For the given path, get the List of all files in the directory tree
+    For the given path, retrieve list of all files in the directory tree
     including those in subdirectories
     """
+    
     # create a list of file and sub directories names in the given directory 
     filelist = os.scandir(path)
     allfiles = list()
@@ -42,6 +50,83 @@ def get_filelist(path):
         else:
             allfiles.append(fullpath)
     return allfiles
+
+
+def read_sims(files):
+    """
+    For the given file path of maizsim model output .txt files, 
+    - 1. slightly resets column names for model output
+    - 2. fetches year/site/cvar info from file name
+    - 3. reads in last line of model output
+    - 4. documents files with abnormal line output length and does not read them
+    - 5. compiles year/site/cvar info & last line of model output into pd.DataFrame
+    - 6. issues compiled into pd.DataFrame as well
+    - 7. returns: df_sims & df_issues
+    """
+    
+    cols = ['year', 'cvar', 'site', 'date', 'jday', 'time', 
+            'leaves', 'mature_lvs', 'drop_lvs', 'LA', 'LA_dead', 'LAI', 
+            'RH', 'leaf_WP', 'PFD', 'solrad', 'temp_soil', 'temp_air', 'temp_can', 'ET_dmd', 'ET_suply',
+            'Pn', 'Pg', 'resp', 'av_gs', 'LAI_sunlit', 'LAI_shaded', 'PFD_sunlit', 'PFD_shaded',
+            'An_sunlit', 'An_shaded', 'Ag_sunlit', 'Ag_shaded', 'gs_sunlit', 'gs_shaded', 'VPD',
+            'N', 'N_dmd', 'N_upt', 'N_leaf', 'PCRL', 'dm_total', 'dm_shoot', 'dm_ear', 'dm_totleaf',
+            'dm_dropleaf', 'df_stem', 'df_root', 'roil_rt', 'mx_rootdept', 
+            'available_water', 'soluble_c', 'note']
+
+    years = []
+    cvars = []
+    sites = []
+    data_all = []
+    issues = []
+    
+    for file in files:
+        # extrating basic file info
+        year = int(file.split('/')[-3])
+        site = file.split('/')[-1].split('_')[1]
+        cvar = int(file.split('/')[-1].split('_')[-1].split('.')[0])
+
+        # reading in file and setting up structure
+        with open(file, 'r') as f:
+            f.seek(0, os.SEEK_END) # moving the pointer to the very end of the file
+                                   # * f.seek(offset, whence)
+                                   # * The position is computed from adding offset to a reference point,
+                                   # * the reference point is selected by the whence argument.
+                                   # * os.SEEK_SET (=0)
+                                   # * os.SEEK_CUR (=1)
+                                   # * os.SEEK_END (=2)
+            try: 
+                f.seek(f.tell() - 3000, os.SEEK_SET) # finding the current position -
+                                                     # (should be at the very end of the file)
+                                                     # and counting back a few positions 
+                                                     # and reading forward from there
+                                    # * f.tell() returns an integer giving the file object’s 
+                                    # * current position in the file represented as number of bytes 
+                                    # * from the beginning of the file when in binary mode 
+                                    # * and an opaque number when in text mode.
+                for line in f:
+                    f_content = f.readlines()
+
+                if len(f_content[-1]) == 523: # character length of a normal output
+                    sim_output = list(f_content[-1].split(','))
+                    data = [i.strip() for i in sim_output]
+                    data.insert(0, year)
+                    data.insert(1, cvar)
+                    data.insert(2, site)
+                    data_all.append(data)
+
+                else: 
+                    issues.append(file)
+
+            except:
+                print(file)
+                
+    df_sims = pd.DataFrame(data_all, columns=cols)
+    df_sims.dm_total = df_sims.dm_total.astype(float)
+    df_sims.dm_ear = df_sims.dm_ear.astype(float)
+    df_issues = pd.Series(issues, dtype='str')
+    
+    return df_sims, df_issues
+    
 
 def CC_VPD(temp, rh):
     """
