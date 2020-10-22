654c8ea1b7f178cb2511fb19c72316df435d9d1e
Jennifer Hsiao <etismeimet@gmail.com>
2020-10-21 20:49:54 -0700

    gdd calculations for dynamic pdate

--- notebooks/process_sims.py
+++ notebooks/process_sims.py
@@ -1,7 +1,13 @@
 import pandas as pd
 from funcs import get_filelist, read_sims
 
+# get_filelist: for the given path, retrieve list of all files within the directory tree, including those in subdirectories
 filelist = get_filelist('/home/disk/eos8/ach315/upscale/sims/opt/')
 files = filelist[:]
+
+# read_sims: read in last line of maizsim simulation output, organize and form into pd.DataFrame
+# outputs: df_sims
 df_sims, df_issues = read_sims(files)
-df_sims.to_csv('/home/disk/eos8/ach315/upscale/data/sims_6105.csv', index=False)
+
+# store output since processing raw maizsim outputs take quite some time
+#df_sims.to_csv('/home/disk/eos8/ach315/upscale/data/sims_6105.csv', index=False)
